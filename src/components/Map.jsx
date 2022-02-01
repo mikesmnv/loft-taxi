caf6a234f7bcf82207096607264aa7697e5aaae5
Mikhail Smirnov <mikhailsmv89@gmail.com>
2022-02-01 23:51:14 +0300

    Update Map.jsx

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,41 @@
 import React, {Component} from 'react'
 import mapboxgl from 'mapbox-gl'
+import {connect} from "react-redux"
 
 class Map extends Component {
-    map= null
+    map = null;
     mapContainer = React.createRef();
+
+    drawRoute = (map, coordinates) => {
+        map.flyTo({
+          center: coordinates[0],
+          zoom: 15
+        });
+       
+        map.addLayer({
+          id: "route",
+          type: "line",
+          source: {
+            type: "geojson",
+            data: {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "LineString",
+                coordinates
+              }
+            }
+          },
+          layout: {
+            "line-join": "round",
+            "line-cap": "round"
+          },
+          paint: {
+            "line-color": "#ffc617",
+            "line-width": 8
+          }
+        });
+      };
 
     componentDidMount() {
         mapboxgl.accessToken="pk.eyJ1IjoibWlraGFpbHNtdjg5IiwiYSI6ImNreG5ib3ZwaDI2dmYycGtqcDJ0eGxhMHMifQ.N-xkZFCKg3Z_1S-RB6OX7A";
@@ -13,6 +45,13 @@ class Map extends Component {
             center: [30.3056504, 59.9429126],
             zoom: 10,
         })
+    }
+
+    componentDidUpdate() {
+        console.log(this.props.route);
+        if (this.props.route.length !== 0) {
+            this.drawRoute(this.map, this.props.route);
+        }
     }
 
     componentWillUnmount(){
@@ -26,4 +65,10 @@ class Map extends Component {
     }
 }
 
-export default Map;
+
+const MapWithConnect = connect(
+    (state) => ({route: state.route.route}),
+    null
+) (Map)
+
+export default MapWithConnect;
